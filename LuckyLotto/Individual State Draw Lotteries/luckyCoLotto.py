dd44326ddb756871dfb6304a02e0053c5aaa0eb6
Joe Jones <bebop828jj@gmail.com>
2025-05-06 21:49:46 -0400

    completed a file

--- LuckyLotto/Individual State Draw Lotteries/luckyCoLotto.py
+++ LuckyLotto/Individual State Draw Lotteries/luckyCoLotto.py
@@ -9,7 +9,7 @@
 import random
 
 # Colorado Game Choices
-def powerballl(): 
+def powerball(): 
     pb_main = sorted(random.sample(range(1, 70), 5))
     pb_rb = random.sample(range(1, 27), 1)[0]
     pb_luck = f"Powerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
@@ -23,8 +23,8 @@ def mega_millions():
     main_num = sorted(random.sample(range(1, 71), 5))
     megaball = random.sample(range(1, 26), 1)[0]
     mm_numbers_main = f"Mega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
-    mm_drawings = f"Base ticket price $2. Cut-off time to play is 8pm MT night of drawings. Drawings are held Tues. Fri. 9pm MT."
-    mm_add = f"Available add on- Megaplier $1 extra."
+    mm_drawings = f"Base ticket price $5. Cut-off time to play is 8pm MT night of drawings. Drawings are held Tues. Fri. 9pm MT."
+    mm_add = f"Megaplier comes with every ticket purchase."
     mm_rules = f"Official Rules and Play can be found- https://www.megamillions.com Good Luck!"    
     return (mm_numbers_main, mm_drawings, mm_add, mm_rules)
 
@@ -40,13 +40,108 @@ def lucky_for_life_co():
 
 def co_lotto():
     co_main = sorted(random.sample(range(1, 41), 6))
-    co_return = f"Colorado Lotto+ Lucky Numbers: {co_main}."
+    co_return = f"Colorado Lotto+ Lucky Numbers: {co_main}"
     co_draw = f"Base ticket cost $2. Top Prize up to $250,000. Drawings held Mon, Wed, Sat."
-    co_add = f"Available add-on- Play Plus $1"
+    co_add = f"Available add on- Play Plus $1"
     co_rules = f"Official Rules and Play can be found- https://www.coloradolottery.com/en/games/lotto/ Good Luck!"
     return(co_return, co_draw, co_add, co_rules)
 
 
+def cash5(): 
+    cash5_num = sorted(random.sample(range(1, 33), 5))
+    cash5_return = f"Cash 5 Lucky Numbers: {cash5_num}"
+    cash5_draw = f"Base ticket cost $1. Drawings are held nightly."
+    cash5_add = f"Available add on- EZ Match $1 extra"
+    cash5_rules = f"Official Rules and Play can be found- https://www.coloradolottery.com/en/games/cash5/"
+    return (cash5_return, cash5_draw, cash5_add, cash5_rules)
+
+
+def pick_3():
+    while True: 
+        print("Select a game time to play:")
+        print("1. Mid-day Draw")
+        print("2. Evening Draw")
+
+        try:
+            choice = int(input("Make selection and enter either 1 or 2: ").strip())
+        except ValueError:
+            print("Invalid input. Please enter either 1 or 2")
+            continue
+
+        game_time_mapping = {
+            1: 'Mid-Day',
+            2: 'Evening'
+        }    
+
+        if choice not in game_time_mapping:
+            print("Invalid selection. Please choose 1 or 2")
+            continue
+
+        game_time = game_time_mapping[choice]
+        break
+    set_1 = random.sample(range(1, 10), 1)
+    set_2 = random.sample(range(1, 10), 1)   
+    set_3 = random.sample(range(0, 10), 1)
+    p3_main = f"Pick 3 Lucky Numbers: {set_1, set_2, set_3}"
+    p3_now = f"Now decide if you play these numbers Exact, Any Order, Exact/Any Order, Front Pair, or Back Pair"
+    p3_drawings = f"Ticket bet prices- $.50, $1, $2, or $5"      
+    p3_rules = f"Official Rules and Play can be found- https://www.coloradolottery.com/en/games/pick3/ Good Luck!"    
+    return (p3_main, p3_now, p3_drawings, p3_rules)
+
+
+co_lotto_draw_games = {
+    1: powerball,
+    2: mega_millions,
+    3: lucky_for_life_co,
+    4: co_lotto,
+    5: cash5,
+    6: pick_3
+    
+}
+
+
+def play_game():
+    print("Colorado Lotto Game Choices")
+    print("1. Powerball")
+    print("2. Mega Millions")
+    print("3. Lucky for Life")
+    print("4. Colorado Lotto")
+    print("5. Cash 5")
+    print("6. Pick 3")    
+
+
+    while True:
+            try:
+                co_lotto_game_choice = int(input("Which Lucky Game would you like to try: "))
+                if co_lotto_game_choice in co_lotto_draw_games:
+                    for result in co_lotto_draw_games[co_lotto_game_choice]():
+                        print(result)
+                    break 
+                else:
+                    print("Not a valid option. Please try again.")
+            except ValueError:  
+                print("Not a valid option. Please try again.")
+
+   
+
+def main():
+    while True:
+        play_game()
+        while True:
+            play_again = input("\nWould you like to try another draw game? (y/n): ").strip().lower()
+            if play_again == 'y':
+                break  
+            elif play_again == 'n':
+                print("Ok! Good Luck and Go WIN Big!!!")
+                return   
+            else:
+                print("That is not an acceptable response. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
+
+    
 
 
 
