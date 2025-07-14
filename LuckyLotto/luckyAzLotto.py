ef0c3f45f62772980f324f3d11f5712d6de6352a
Joe Jones <bebop828jj@gmail.com>
2025-07-14 14:11:37 -0400

    Update 1 completed for repo file 2025/07/14

--- LuckyLotto/luckyAzLotto.py
+++ LuckyLotto/luckyAzLotto.py
@@ -3,7 +3,7 @@
 ###########################################################################
 
 ###########################################################################
-# last file update- 2025/05/26
+# last file update- 2025/07/14
 ########################################################################### 
 
 import random
@@ -12,51 +12,51 @@ import random
 def powerball():
     pb_main = sorted(random.sample(range(1, 70), 5))
     pb_rb = random.randint(1, 26)
-    pb_luck = f"Powerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
-    pb_draw = f"Base ticket price $2. Cut-off time to play in Arizona varies throughout the year. Drawings are held Mon. Wed. & Sat." 
-    pb_add = f"Available add on- Power Play $1 extra."
-    pb_official = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/powerball/ Good Luck!"    
+    pb_luck = f"\nPowerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
+    pb_draw = "Ticket price to play $2. Drawings are held Mon. Wed. & Sat." 
+    pb_add = "Available add on- Power Play $1 extra."
+    pb_official = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/powerball/ Good Luck!"    
     return (pb_luck, pb_draw, pb_add, pb_official)
 
 def mega_millions():
     main_num = sorted(random.sample(range(1, 71), 5))
     megaball = random.randint(1, 25)
-    mm_numbers_main = f"Mega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
-    mm_drawings = f"Ticket price $5. Cut-off time to play in Arizona varies throughout the year. Drawings are held Tues and Fri."
-    mm_add = f"Megaplier included with each Gameplay."
-    mm_rules = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/mega-millions/ Good Luck!"    
+    mm_numbers_main = f"\nMega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
+    mm_drawings = "Ticket price to play $5. Drawings are held Tues & Fri."
+    mm_add = "Megaplier included with each Gameplay."
+    mm_rules = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/mega-millions/ Good Luck!"    
     return (mm_numbers_main, mm_drawings, mm_add, mm_rules)
 
 def the_pick():
     pick_num = sorted(random.sample(range(1, 45), 6))
-    pick_num_main = f"The Pick Lucky Numbers: {pick_num}"
-    pick_draw = f"Base ticket price $1. Cut-off time to play is 6:59pm MST. Drawings are held Mon, Wed, Sat."
-    pick_add = f"Available add ons- EXTRA $1 or $2 option."
-    pick_rules = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/the-pick/ Good Luck!"
+    pick_num_main = f"\nThe Pick Lucky Numbers: {pick_num}"
+    pick_draw = "Base ticket price $1. Drawings are held Mon. Wed. & Sat."
+    pick_add = "Available add ons- EXTRA $1 or $2 option."
+    pick_rules = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/the-pick/ Good Luck!"
     return (pick_num_main, pick_draw, pick_add, pick_rules)
 
 def triple_twist():
     trip_num = sorted(random.sample(range(1, 43), 6))
-    trip_num_main = f"Triple Twist Lucky Numbers: {trip_num}"
-    trip_draw = f"Ticket Cost $2. Cut-off time to play is 6:59pm MST. Drawings are held Mon-Sun."
-    trip_rules = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/triple-twist/ Good Luck!"
+    trip_num_main = f"\nTriple Twist Lucky Numbers: {trip_num}"
+    trip_draw = "Ticket Cost to play $2. Drawings held Daily!"
+    trip_rules = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/triple-twist/ Good Luck!"
     return (trip_num_main, trip_draw, trip_rules)
 
 def fantasy5():
     fan5 = sorted(random.sample(range(1, 42), 5))
-    fan5_num_main = f"Fantasy 5 Lucky Numbers: {fan5}"
-    fan5_draw = f"Base ticket price $1. Cut-off time to play is 6:59pm MST. Drawings are held Mon-Sun"
-    fan5_rules = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/fantasy-5/ Good Luck!"
+    fan5_num_main = f"\nFantasy 5 Lucky Numbers: {fan5}"
+    fan5_draw = "Ticket price to play $1. Drawings held Daily!"
+    fan5_rules = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/fantasy-5/ Good Luck!"
     return (fan5_num_main, fan5_draw, fan5_rules)
 
 def pick3():    
     set_1 = random.randint(0, 9)
     set_2 = random.randint(0, 9) 
     set_3 = random.randint(0, 9)    
-    pk3_main = f"Pick 3 Lucky Numbers: {set_1, set_2, set_3}"   
-    pk3_now = f"Now you have to decide to play these numbers Straight, Box, Straight/Box, Front Pair, or Back Pair."
-    pk3_draw = f"Ticket price $1. Cut-off time for play is 6:59pm MST. Drawings are held Mon-Sun"
-    pk3_rules = f"Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/pick-3/ Good Luck!"
+    pk3_main = f"\nPick 3 Lucky Numbers: {set_1, set_2, set_3}"   
+    pk3_now = "Now you have to decide to play these numbers Straight, Box, Straight/Box, Front Pair, or Back Pair."
+    pk3_draw = "Ticket price to play $1. Drawings held Daily!"
+    pk3_rules = "Official Rules and Play can be found- https://www.arizonalottery.com/draw-games/pick-3/ Good Luck!"
     return (pk3_main, pk3_now, pk3_draw, pk3_rules)
 
 
@@ -114,7 +114,7 @@ az_lotto_draw_games = {
 
 #the menu
 def play_game():
-    print("Arizona Lotto Game Choices")
+    print("\nArizona Lotto Game Choices")
     print("1. Powerball")
     print("2. Mega Millions")
     print("3. The Pick")
@@ -127,7 +127,7 @@ def play_game():
     #waiter
     while True:
         try:
-            az_lotto_game_choice = int(input("Which Lucky Game would you like to try: "))
+            az_lotto_game_choice = int(input("\nWhich Lucky Game would you like to try: "))
             if az_lotto_game_choice in az_lotto_draw_games:                        
                 result = az_lotto_draw_games[az_lotto_game_choice]()
 
