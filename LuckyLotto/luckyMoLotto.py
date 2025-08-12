c41ef855bed59b5c287e81c39c93cca63a3350a4
Joe Jones <bebop828jj@gmail.com>
2025-08-12 14:13:16 -0400

    Update 1 for repo file completed 2025/08/12

--- LuckyLotto/luckyMoLotto.py
+++ LuckyLotto/luckyMoLotto.py
@@ -3,7 +3,7 @@
 ###########################################################################
 
 ###########################################################################
-# last file update- 2025/06/01
+# last file update- 2025/08/12
 ###########################################################################
 
 
@@ -13,8 +13,8 @@ import random
 def powerball():
     pb_main = sorted(random.sample(range(1, 70), 5))
     pb_rb = random.randint(1, 26)
-    pb_luck = f"Powerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
-    pb_draw = f"Base ticket price $2. Drawings held Mon, Wed, Sat 9:59pm CT in Tallahassee, Fl. by the Multi-State Lottery Association." 
+    pb_luck = f"\nPowerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
+    pb_draw = f"Ticket price to play $2. Drawings held Mon. Wed. & Sat. 9pm CT" 
     pb_add = f"Available add ons- Power Play and Double Play each for $1 extra."
     pb_official = f"Official Rules and Play can be found- https://www.molottery.com/powerball Good Luck!"    
     return (pb_luck, pb_draw, pb_add, pb_official)
@@ -23,8 +23,8 @@ def powerball():
 def mega_millions():
     main_num = sorted(random.sample(range(1, 71), 5))
     megaball = random.randint(1, 25)
-    mm_numbers_main = f"Mega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
-    mm_drawings = f"Base ticket price $5. Drawings are held Tues. Fri. 10pm CT."
+    mm_numbers_main = f"\nMega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
+    mm_drawings = f"Ticket price to play $5. Drawings are held Tues. & Fri. 10pm CT."
     mm_add = f"Megaplier available with every play."
     mm_rules = f"Official Rules and Play can be found- https://www.molottery.com/mega-millions Good Luck!"    
     return (mm_numbers_main, mm_drawings, mm_add, mm_rules)
@@ -33,17 +33,17 @@ def mega_millions():
 def cash_4_life_mo():
     cash_main = sorted(random.sample(range(1, 61), 5))
     cash_ball = random.randint(1, 4)
-    numbers_main = f"Cash 4 Life Lucky Numbers: {cash_main}, Cash Ball: {cash_ball}."
+    numbers_main = f"\nCash 4 Life Lucky Numbers: {cash_main}, Cash Ball: {cash_ball}."
     cash_add = f"Available add-on- EZ Match for $1 extra per play"
-    drawings_c4l = f"Base ticket price $2. Drawings are held Nightly 8pm CT."    
+    drawings_c4l = f"Ticket price to play $2. Drawings are held Nightly 8pm CT."    
     rules_c4l = f"Official Rules and Play can be found- https://www.molottery.com/cash4life Good Luck!"    
-    return (numbers_main, cash_add, drawings_c4l, rules_c4l)
+    return (numbers_main, drawings_c4l, cash_add, rules_c4l)
 
 
 def mo_lotto():
     lotto_main = sorted(random.sample(range(1, 45), 6))
-    lotto_num_main = f"Missouri Lucky Lotto Numbers: {lotto_main}" 
-    lotto_drawings = f"Base ticket price $1. Drawings are Wed. Sat. at 8:59pm CT."
+    lotto_num_main = f"\nMissouri Lucky Lotto Numbers: {lotto_main}" 
+    lotto_drawings = f"Ticket price to play $1. Drawings are Wed. & Sat. 8:59pm CT."
     lotto_add = f"Available add on- EZ Match for $1 extra per play"
     lotto_rules = f"Official Rules and Play can be found- https://www.molottery.com/lotto Good Luck!"
     return (lotto_num_main, lotto_drawings, lotto_add, lotto_rules)
@@ -51,8 +51,8 @@ def mo_lotto():
 
 def show_lotto():
     show_main = sorted(random.sample(range(1, 40), 5))
-    show_num_main = f"Missouri Lucky Show Me Cash Lotto Numbers: {show_main}"
-    show_drawings = f"Base ticket price $1. Drawings held daily at 8:59pm CT."
+    show_num_main = f"\nMissouri Lucky Show Me Cash Lotto Numbers: {show_main}"
+    show_drawings = f"Ticket price to play $1. Drawings held Daily 8:59pm CT."
     show_add = f"Available add on- EZ Match for $1 extra per play."
     show_rules = f"Official Rules and Play can be found- https://www.molottery.com/show-me-cash Good Luck!"
     return (show_num_main, show_drawings, show_add, show_rules)
@@ -60,9 +60,9 @@ def show_lotto():
 
 def pick_4():
     while True: 
-        print("Select a game time to play:")
-        print("1. Mid-day 12:45pm CT Draw")
-        print("2. Evening 8:59pm CT Draw")
+        print("\nSelect a game time to play:")
+        print("1. Mid-day")
+        print("2. Evening")
 
         try:
             choice = int(input("Make selection and enter either 1 or 2: ").strip())
@@ -85,9 +85,9 @@ def pick_4():
     set_2 = random.randint(0, 9)
     set_3 = random.randint(0, 9)
     set_4 = random.randint(0, 9)
-    p4_main = f"Pick 4 Lucky Numbers for {game_time}: {set_1, set_2, set_3, set_4}"
-    p4_now = f"Multiple ways to Play and Win!"
-    p4_drawings = f"Base ticket prices are $.50 or $1."
+    p4_main = f"\nPick 4 Lucky Numbers for {game_time}: {set_1, set_2, set_3, set_4}"
+    p4_now = f"Multiple ways to Play and Win!!!"
+    p4_drawings = f"Ticket prices for play- $.50 or $1."
     p4_fire_mid = f"Available add on- EZ Match for $1 extra per play or Wild Ball (doubles the ticket price)."    
     p4_rules = f"Official Rules and Play can be found- https://www.molottery.com/pick4 Good Luck!"
     return (p4_main, p4_now, p4_drawings, p4_fire_mid, p4_rules)
@@ -95,9 +95,9 @@ def pick_4():
 
 def pick_3():
     while True: 
-        print("Select a game time to play:")
-        print("1. Mid-day 12:45pm CT Draw")
-        print("2. Evening 8:59pm CT Draw")
+        print("\nSelect a game time to play:")
+        print("1. Mid-day")
+        print("2. Evening")
 
         try:
             choice = int(input("Make selection and enter either 1 or 2: ").strip())
@@ -119,9 +119,9 @@ def pick_3():
     set_1 = random.randint(0, 9)
     set_2 = random.randint(0, 9)
     set_3 = random.randint(0, 9)
-    p3_main = f"Pick 4 Lucky Numbers for {game_time}: {set_1, set_2, set_3}"
-    p3_now = f"Multiple ways to Play and Win!"
-    p3_drawings = f"Base ticket prices are $.50 or $1."
+    p3_main = f"\nPick 4 Lucky Numbers for {game_time}: {set_1, set_2, set_3}"
+    p3_now = f"Multiple ways to Play and Win!!!"
+    p3_drawings = f"Ticket prices for play- $.50 or $1."
     p3_fire_mid = f"Available add on- EZ Match for $1 extra per play or Wild Ball (doubles the ticket price)."    
     p3_rules = f"Official Rules and Play can be found- https://www.molottery.com/pick3 Good Luck!"
     return (p3_main, p3_now, p3_drawings, p3_fire_mid, p3_rules)
@@ -193,7 +193,7 @@ mo_lotto_draw_games = {
 
 # print the menu
 def play_game():
-    print("Missouri Lotto Game Choices")
+    print("\nMissouri Lotto Game Choices")
     print("1. Powerball")
     print("2. Mega Millions")
     print("3. Cash 4 Life")   
@@ -207,7 +207,7 @@ def play_game():
     #waiter
     while True:
         try:
-            mo_lotto_game_choice = int(input("Which Lucky Game would you like to try: "))
+            mo_lotto_game_choice = int(input("\nWhich Lucky Game would you like to try: "))
             if mo_lotto_game_choice in mo_lotto_draw_games:                        
                 result = mo_lotto_draw_games[mo_lotto_game_choice]()
 
