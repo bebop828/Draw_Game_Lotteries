b36ecd3d379e11c19b0b52658551357dc7fd9958
Joe Jones <bebop828jj@gmail.com>
2025-08-12 09:38:28 -0400

    Update 1 for repo file completed 2025/08/12

--- LuckyLotto/luckyMsLotto.py
+++ LuckyLotto/luckyMsLotto.py
@@ -3,7 +3,7 @@
 ###########################################################################
 
 ###########################################################################
-# last file update- 2025/06/01
+# last file update- 2025/08/12
 ###########################################################################
 
 
@@ -13,8 +13,8 @@ import random
 def powerball():
     pb_main = sorted(random.sample(range(1, 70), 5))
     pb_rb = random.randint(1, 26)
-    pb_luck = f"Powerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
-    pb_draw = f"Base ticket price $2. Drawings are held Mon. Wed. & Sat. 9:59pm CT." 
+    pb_luck = f"\nPowerball Lucky Numbers: {pb_main}, Powerball: {pb_rb}." 
+    pb_draw = f"Ticket price to play $2. Drawings are held Mon. Wed. & Sat. 9:59pm CT." 
     pb_add = f"Available add ons- Power Play and Double Play each for $1 extra."
     pb_official = f"Official Rules and Play can be found- https://www.mslottery.com/games/powerball/ Good Luck!"    
     return (pb_luck, pb_draw, pb_add, pb_official)
@@ -22,8 +22,8 @@ def powerball():
 def mega_millions():
     main_num = sorted(random.sample(range(1, 71), 5))
     megaball = random.randint(1, 25)
-    mm_numbers_main = f"Mega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
-    mm_drawings = f"Base ticket price $5. Drawings are held Tues. Fri. 10pm CT."
+    mm_numbers_main = f"\nMega Millions Lucky Numbers: {main_num}, Mega Ball: {megaball}"
+    mm_drawings = f"Ticket price to play $5. Drawings are held Tues. & Fri. 10pm CT."
     mm_add = f"Megaplier available with every play."
     mm_rules = f"Official Rules and Play can be found- https://www.megamillions.com Good Luck!"    
     return (mm_numbers_main, mm_drawings, mm_add, mm_rules)
@@ -32,16 +32,16 @@ def mega_millions():
 def lotto_america(): 
     lotto_main = sorted(random.sample(range(1, 53), 5))
     star_ball = random.randint(1, 10)
-    lotto_num = f"Lucky Lotto America with EZ-Match Numbers: {lotto_main}, Star Ball: {star_ball}."
-    lotto_drawings = f"Base ticket price $1. Drawings are held Mon. Wed. and Sat. 9:15pm CT"    
+    lotto_num = f"\nLucky Lotto America with EZ-Match Numbers: {lotto_main}, Star Ball: {star_ball}."
+    lotto_drawings = f"Ticket price to play $1. Drawings are held Mon. Wed. & Sat. 9:15pm CT"    
     lotto_add = f"Available add on games- EZ-Match and All Star Bonus $1 extra each per play"
     lotto_rules = f"Official Rules and Play can be found- https://www.mslottery.com/games/lotto-america Good Luck!"    
     return (lotto_num, lotto_drawings, lotto_add, lotto_rules)
 
 def match_5multi(): 
     match_main = sorted(random.sample(range(1, 38), 5))
-    match_numbers_main = f"Lucky Match 5 with Multiplier Numbers: {match_main}"
-    match_drawings = f"Base ticket price $2. Drawings held Daily 9:30pm CT."
+    match_numbers_main = f"\nLucky Match 5 with Multiplier Numbers: {match_main}"
+    match_drawings = f"Ticket price to play $2. Drawings held Daily 9:30pm CT."
     match_add = f"Available add on- Multiplier $1 extra"
     match_rules = f"Official Rules and Play can be found- https://www.mslottery.com/games/mm5 Good Luck!"
     return (match_numbers_main, match_drawings, match_add, match_rules)
@@ -49,9 +49,9 @@ def match_5multi():
 
 def cash_4():
     while True: 
-        print("Select a game time to play:")
-        print("1. Mid-Day 3pm CT Draw")
-        print("2. Evening 10pm CT Draw")
+        print("\nSelect a game time to play:")
+        print("1. Mid-Day")
+        print("2. Evening")
         
         try:
             choice = int(input("Make selection and enter (1-3): ").strip())
@@ -75,9 +75,9 @@ def cash_4():
     set_2 = random.randint(0, 9) 
     set_3 = random.randint(0, 9)
     set_4 = random.randint(0, 9)
-    c4_main = f"Cash 4 with Fireball Lucky Numbers for {game_time}: {set_1, set_2, set_3, set_4}"
-    c4_now = f"Now you have to decide to play these numbers Exact, Any, Exact/Any, or Combo."
-    c4_drawings = f"Base ticket prices are $.50 or $1."      
+    c4_main = f"\nCash 4 with Fireball Lucky Numbers for {game_time}: {set_1, set_2, set_3, set_4}"
+    c4_now = f"Multiple ways to Play and Win!!!"
+    c4_drawings = f"Ticket prices for play- $.50 or $1."      
     c4_add = f"Available add on- FIREBALL (doubles the ticket price)"
     c4_rules = f"Official Rules and Play can be found- https://www.mslottery.com/games/cash-4/ Good Luck!"    
     return (c4_main, c4_now, c4_drawings, c4_add, c4_rules)
@@ -85,9 +85,9 @@ def cash_4():
 
 def cash_3():
     while True: 
-        print("Select a game time to play:")
-        print("1. Mid-Day 3pm CT Draw")
-        print("2. Evening 10pm CT Draw")
+        print("\nSelect a game time to play:")
+        print("1. Mid-Day")
+        print("2. Evening")
         
         try:
             choice = int(input("Make selection and enter (1-3): ").strip())
@@ -110,9 +110,9 @@ def cash_3():
     set_1 = random.randint(0, 9)
     set_2 = random.randint(0, 9) 
     set_3 = random.randint(0, 9)
-    c3_main = f"Cash 3 with Fireball Lucky Numbers for {game_time}: {set_1, set_2, set_3}"
-    c3_now = f"Now you have to decide to play these numbers Exact, Any, Exact/Any, or Combo."
-    c3_drawings = f"Base ticket prices are $.50 or $1."      
+    c3_main = f"\nCash 3 with Fireball Lucky Numbers for {game_time}: {set_1, set_2, set_3}"
+    c3_now = f"Multiple ways for you to Play and Win!!!"
+    c3_drawings = f"Ticket prices for play- $.50 or $1."      
     c3_add = f"Available add on- FIREBALL (doubles the ticket price)"
     c3_rules = f"Official Rules and Play can be found-https://www.mslottery.com/games/cash-3/ Good Luck!"    
     return (c3_main, c3_now, c3_drawings, c3_add, c3_rules)
@@ -179,7 +179,7 @@ ms_lotto_draw_games = {
 
 # print the menu
 def play_game():
-    print("Mississippi Lotto Game Choices")
+    print("\nMississippi Lotto Game Choices")
     print("1. Powerball")
     print("2. Mega Millions")
     print("3. Lotto America with EZ-Match")
@@ -192,7 +192,7 @@ def play_game():
     #waiter
     while True:
         try:
-            ms_lotto_game_choice = int(input("Which Lucky Game would you like to try: "))
+            ms_lotto_game_choice = int(input("\nWhich Lucky Game would you like to try: "))
             if ms_lotto_game_choice in ms_lotto_draw_games:                        
                 result = ms_lotto_draw_games[ms_lotto_game_choice]()
 
